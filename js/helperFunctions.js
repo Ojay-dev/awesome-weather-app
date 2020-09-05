de99fa4f97e385e233d53f97e4125ceaf56b8027
Alikali Ojonugwa <alikali.ojonugwajustice@gmail.com>
2020-09-05 01:59:59 +0100

    Mojar restructuring

--- js/helperFunctions.js
+++ js/helperFunctions.js
@@ -2,8 +2,24 @@ export function setInnerHTML(selector, content) {
   document.querySelector(selector).innerHTML = content;
 }
 
-export function url(location) {
-  return `http://api.openweathermap.org/data/2.5/weather?q=${location}&units=metric&appid=9bcd3d9f9f02d99878e8189061742670`;
+export async function ParseLocation(location) {
+  let response;
+  try {
+    response = await fetch(
+      `https://api.locationiq.com/v1/autocomplete.php?key=37af076ab6193a&q=${location}&format=json`
+    );
+    response = await response.json();
+  } catch (error) {
+    console.error("Error, ", error);
+  }
+  // console.log(response[0]);
+  return await {
+    latitude: response[0].lat,
+    longitude: response[0].lon,
+    location: response[0].address.name,
+    state: response[0].address.state,
+    country: response[0].address.country,
+  };
 }
 
 export function currentDate() {
@@ -37,4 +53,69 @@ export function currentDate() {
   let year = d.getFullYear();
 
   return `${day}, ${date} ${month} ${year}`;
+}
+
+export function addForecast(dailyForecast) {
+  let forecastSection = document.querySelector(".forecast-section");
+  const forecastDiv = [];
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let d = new Date();
+
+  forecastSection.innerHTML = "";
+
+  for (let i = 1; i < dailyForecast.length - 1; i++) {
+    let forecastDay = d.getDay();
+    if (d.getDay() === 6) {
+      forecastDay = -1;
+    }
+ 
+    forecastDiv[i] = document.createElement("div");
+    forecastDiv[i].classList.add("forecast");
+
+    let sunnyCondition = ["clear", "sun"];
+    let cloudCondition = ["clouds"];
+    let rainCondition = ["rain", "drizzle"];
+
+    let weatherIcon = sunnyCondition.some((str) =>
+      dailyForecast[i].weather[0].description.toLowerCase().includes(str)
+    )
+      ? "./svg/sun.svg"
+      : rainCondition.some((str) =>
+          dailyForecast[i].weather[0].description.toLowerCase().includes(str)
+        )
+      ? "./svg/rain.svg"
+      : "./svg/cloud.svg";
+
+    forecastDiv[i].innerHTML = `
+      <div >
+        <div>
+          <div>
+            <img src=${weatherIcon} alt="rain" />
+
+            <span>${dailyForecast[i].weather[0].description}</span>
+          </div>
+
+          <span class="vr"></span>
+
+          <span class="weather-degree">${Math.round(
+            dailyForecast[i].temp.day
+          )}&deg;C </span>
+        </div>
+
+        <h3>${days[forecastDay + i]}</h3>
+      </div>
+      `;
+  }
+
+  forecastDiv.forEach((div) => {
+    forecastSection.appendChild(div);
+  });
 }
--- js/script.js
+++ js/script.js
@@ -1,41 +1,47 @@
-import { url, setInnerHTML, currentDate } from "./helperFunctions.js";
+import {
+  ParseLocation,
+  setInnerHTML,
+  currentDate,
+  addForecast,
+} from "./helperFunctions.js";
 import countryIso from "./countryISO.js";
 
 const weatherDetails = document.querySelector(".current-weather-details");
 const notFoundErrorMsg = document.querySelector(".not-found");
 const searchForm = document.querySelector("form");
-const preloader = document.querySelector(".loader");
-const weatherDom = document.querySelector("main");
 
 let location;
 
 window.addEventListener("load", () => {
-  document.querySelector(
-    ".current-weather-details h3"
-  ).innerHTML = currentDate();
+  // document.querySelector(
+  //   ".current-weather-details h3"
+  // ).innerHTML = currentDate();
 
   const successfulLookup = async (position) => {
     const { latitude, longitude } = position.coords;
+    // console.log(latitude, longitude);
+    lookUpLocationWeather({ lat: latitude, lon: longitude });
 
-    try {
-      let response = await fetch(
-        `https://api.opencagedata.com/geocode/v1/json?q=${latitude}+${longitude}&key=d03c0184daa143daba2fc0b38cad9cd1`
-      );
-      location = await response.json();
-      console.log(location.results[0].components.city);
-      lookUpLocationWeather(location.results[0].components.city)
-    } catch (error) {
-      console.error("Error, ", error);
-      weatherDetails.style.display = "none";
-      notFoundErrorMsg.style.display = "block";
-      notFoundErrorMsg.innerHTML = "Please enter your location"
-    }
+    // try {
+    //   let response = await fetch(
+    //     // `https://api.opencagedata.com/geocode/v1/json?q=${latitude}+${longitude}&key=d03c0184daa143daba2fc0b38cad9cd1`
+    //     `https://us1.locationiq.com/v1/reverse.php?key=37af076ab6193a&format=json&lat=${latitude}&lon=${longitude}`
+    //   );
+
+    //   location = await response.json();
+    //   // console.log(location)
+    // } catch (error) {
+    //   console.error("Error, ", error);
+    //   weatherDetails.style.display = "none";
+    //   notFoundErrorMsg.style.display = "block";
+    //   notFoundErrorMsg.innerHTML = "Please enter a location";
+    // }
   };
 
   const errorLookup = (error) => {
-    console.log(error);
+    // console.log(error);
     geoLocationErrorHandler();
-  }
+  };
 
   window.navigator.geolocation.getCurrentPosition(
     successfulLookup,
@@ -45,32 +51,47 @@ window.addEventListener("load", () => {
 
 searchForm.addEventListener("submit", (e) => {
   e.preventDefault();
-  console.log(document.querySelector("input[type='search']").value);
+  // console.log(document.querySelector("input[type='search']").value);
   let searchInput = document.querySelector("input[type='search']").value;
 
   lookUpLocationWeather(searchInput);
 });
 
 function geoLocationErrorHandler() {
-  preloader.style.display = "none";
-  weatherDetails.style.display = "none";
-  notFoundErrorMsg.style.display = "block";
-  notFoundErrorMsg.innerHTML = "Please enter your location";
+  // weatherDetails.style.display = "none";
+  // notFoundErrorMsg.style.display = "block";
+  // notFoundErrorMsg.innerHTML = "Please enter a location";
+}
+
+function url(lat, lon) {
+  return `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&%20exclude=minutely,hourly&units=metric&appid=9bcd3d9f9f02d99878e8189061742670`;
 }
 
 async function lookUpLocationWeather(location) {
   try {
-    let response = await fetch(url(location));
+    let response;
+    let locationData = {};
 
-    console.log(response);
-
-    preloader.style.display = "block";
-    weatherDom.style.display = "none";
-
-    if (weatherDetails.style.display === "none") {
-      weatherDetails.style.display = "block";
-      notFoundErrorMsg.style.display = "none";
+    if (typeof location === "object") {
+      response = await fetch(url(location.lat, location.lon));
+      let data = await fetch(
+        `https://us1.locationiq.com/v1/reverse.php?key=37af076ab6193a&format=json&lat=${location.lat}&lon=${location.lon}`
+      );
+      data = await data.json();
+      console.log(data.address.state);
+      locationData = {
+        location: data.address.state,
+        country: data.address.country,
+      };
+    } else {
+      locationData = await ParseLocation(location);
+      response = await fetch(url(locationData.latitude, locationData.latitude));
     }
+
+    // if (weatherDetails.style.display === "none") {
+    //   weatherDetails.style.display = "block";
+    //   notFoundErrorMsg.style.display = "none";
+    // }
 
     if (response.status === 404) {
       weatherDetails.style.display = "none";
@@ -83,48 +104,109 @@ async function lookUpLocationWeather(location) {
 
     if (response.status === 200) {
       response = await response.json();
-      setTimeout(() => {
-        setInnerHTML(".weather-degree>span", Math.round(response.main.temp));
-        setInnerHTML(
-          ".current-weather-details>h4",
-          `${response.name}, ${countryIso[response.sys.country]}`
-        );
-
-        setInnerHTML("#weather-description", response.weather[0].description);
-
-        let sunnyCondition = ["clear", "sun"];
-        let cloudCondition = ["clouds"];
-        let rainCondition = ["rain", "drizzle"];
-
-        if (
-          sunnyCondition.some((str) =>
-            response.weather[0].description.toLowerCase().includes(str)
-          )
-        ) {
-          document
-            .querySelector(".current-weather-details img")
-            .setAttribute("src", "./svg/sun.svg");
-        } else if (
-          rainCondition.some((str) =>
-            response.weather[0].description.toLowerCase().includes(str)
-          )
-        ) {
-          document
-            .querySelector(".current-weather-details img")
-            .setAttribute("src", "./svg/rain.svg");
-        } else {
-          document
-            .querySelector(".current-weather-details img")
-            .setAttribute("src", "./svg/cloud.svg");
-        }
-        preloader.style.display = "none";
-        weatherDom.style.display = "block";
-      }, 1000);
-
       console.log(response);
+
+      let sunnyCondition = ["clear", "sun"];
+      let cloudCondition = ["clouds"];
+      let rainCondition = ["rain", "drizzle"];
+
+      let weatherIcon = sunnyCondition.some((str) =>
+        response.current.weather[0].description.toLowerCase().includes(str)
+      )
+        ? "./svg/sun.svg"
+        : rainCondition.some((str) =>
+            response.current.weather[0].description.toLowerCase().includes(str)
+          )
+        ? "./svg/rain.svg"
+        : "./svg/cloud.svg";
+
+      let currentLocation = document.querySelector(".current-location");
+      let currentWeatherDetails = document.createElement("div");
+      currentWeatherDetails.classList.add("current-weather-details");
+
+      currentWeatherDetails.innerHTML = `
+        <div>
+          <div>
+            <img src=${weatherIcon} alt="rain" />
+
+            <span id="weather-description">rainy</span>
+          </div>
+
+          <span class="vr"></span>
+
+          <span class="weather-degree"> ${Math.round(
+            response.current.temp
+          )}&deg;C </span>
+        </div>
+
+        <h3>${currentDate()}</h3>
+        <h4>${locationData.location}, ${
+        !locationData.state
+          ? ""
+          : locationData.location === locationData.state
+          ? ""
+          : locationData.state + ","
+      }  ${locationData.country}
+        </h4>
+      `;
+
+      currentLocation.innerHTML = "";
+      currentLocation.appendChild(currentWeatherDetails);
+
+      // setTimeout(() => {
+      // setInnerHTML(".weather-degree>span", Math.round(response.current.temp));
+      // setInnerHTML(
+      //   ".current-weather-details>h4",
+      //   `${locationData.location}, ${
+      //     !locationData.state
+      //       ? ""
+      //       : locationData.location === locationData.state
+      //       ? ""
+      //       : locationData.state + ","
+      //   }  ${locationData.country}`
+      // );
+
+      // setInnerHTML(
+      //   "#weather-description",
+      //   response.current.weather[0].description
+      // );
+      // document
+      //   .querySelector(".current-weather-details img")
+      //   .setAttribute("src", "./svg/sun.svg");
+
+      // let sunnyCondition = ["clear", "sun"];
+      // let cloudCondition = ["clouds"];
+      // let rainCondition = ["rain", "drizzle"];
+
+      // if (
+      //   sunnyCondition.some((str) =>
+      //     response.current.weather[0].description.toLowerCase().includes(str)
+      //   )
+      // ) {
+      //   document
+      //     .querySelector(".current-weather-details img")
+      //     .setAttribute("src", "./svg/sun.svg");
+      // } else if (
+      //   rainCondition.some((str) =>
+      //     response.current.weather[0].description.toLowerCase().includes(str)
+      //   )
+      // ) {
+      //   document
+      //     .querySelector(".current-weather-details img")
+      //     .setAttribute("src", "./svg/rain.svg");
+      // } else {
+      //   document
+      //     .querySelector(".current-weather-details img")
+      //     .setAttribute("src", "./svg/cloud.svg");
+      // }
+
+      addForecast(response.daily);
+      // }, 1000);
+
+      // console.log(response);
     }
   } catch (error) {
-    console.log(error.message);
-    geoLocationErrorHandler()
+    // console.log(error.message);
+    geoLocationErrorHandler();
   }
 }
